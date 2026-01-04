ce085a01a805ad257cd31c99b52f04ed65cc13dd
ibrahim halil SOYDAN <ibrahimhalilsoydan@gmail.com>
2026-01-04 07:23:47 +0300

    ejs files have been added

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import express from 'express';
 import path from 'node:path';
 
-
-
 const app = express();
+
+app.set('view engine', 'ejs');
+
 //MIDDLEWARES
 app.use(express.static('public'));
 
-
 app.get('/', (req, res) => {
- 
-  res.sendFile(path.resolve('temp','index.html'))
+  res.render('index');
 });
 
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
